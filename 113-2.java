8df994a74b87dfdc6a07f80386a0d2b315a4d8f0
anyulu <450801626@qq.com>
2020-10-22 15:10:17 -0700

    Path Sum II

--- 113-2.java
+++ 113-2.java
@@ -14,15 +14,15 @@
  * }
  */
 class Solution {
+    List<Integer> path = new ArrayList<>();
+    int currSum = 0;
     public List<List<Integer>> pathSum(TreeNode root, int sum) {
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
-        int currSum = 0;
-        dfs(res, root, sum, path, currSum);
+        dfs(res, root, sum);
         return res;
     }
 
-    private void dfs(List<List<Integer>> res, TreeNode root, int sum, List<Integer> path, int currSum) {
+    private void dfs(List<List<Integer>> res, TreeNode root, int sum) {
         if (root == null) return;
         currSum += root.val;
         path.add(root.val);
@@ -34,12 +34,14 @@ class Solution {
             return;
         }
         if (root.left != null) {
-            dfs(res, root.left, sum, path, currSum);
+            dfs(res, root.left, sum);
             path.remove(path.size()-1);
+            currSum -= root.left.val;
         }
         if (root.right != null) {
-            dfs(res, root.right, sum, path, currSum);
+            dfs(res, root.right, sum);
             path.remove(path.size()-1);
+            currSum -= root.right.val;
         }
     }
 }
