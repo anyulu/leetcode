4c68f99883cddc1867520f8ac867e5addd63525f
anyulu <450801626@qq.com>
2020-10-25 11:50:16 -0700

    Max Consecutive Ones III

---
+++ 1004.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestOnes(int[] arr, int k) {
+        if (arr.length <= k) return arr.length;
+        int start = 0;
+        int repeat = 0;
+        int curr = 0;
+        int max = Integer.MIN_VALUE;
+        for (int end = 0; end < arr.length; end++) {
+            if (arr[end] == 1) curr++;
+            repeat = Math.max(curr, repeat);
+            while (end-start+1 > k+repeat) {
+                if (arr[start] == 1) curr--;
+                start++;
+            }
+            max = Math.max(max, end-start+1);
+        }
+        return max;
+    }
+}
