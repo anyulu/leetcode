0e85997189da3e8b0c0f97fb5f49200d6dbe7a04
anyulu <450801626@qq.com>
2020-11-18 17:48:58 -0800

    Best Time to Buy and Sell Stock with Cooldown

---
+++ 309.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) return 0;
+        int[][] dp = new int[prices.length][3];
+        dp[0][0] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][2]-prices[i]);
+            dp[i][1] = dp[i-1][0]+prices[i];
+            dp[i][2] = Math.max(dp[i-1][1], dp[i-1][2]);
+        }
+        return Math.max(dp[prices.length-1][1], dp[prices.length-1][2]);
+    }
+}
