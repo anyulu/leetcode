2611a7a89412d333d867f7f0119abf9d801ab84f
anyulu <450801626@qq.com>
2020-11-22 15:36:57 -0800

    Invert Binary Tree

---
+++ 226.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode res = new TreeNode(root.val);
+        res.left = invertTree(root.right);
+        res.right = invertTree(root.left);
+        return res;
+    }
+}
