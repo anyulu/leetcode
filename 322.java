ba447bbad8b1c7e26b728f89707073d07743d43f
anyulu <450801626@qq.com>
2020-11-22 17:21:07 -0800

    Coin Change

--- 322.java
+++ 322.java
@@ -3,17 +3,18 @@ class Solution {
         if (amount == 0) return 0;
         int[][] dp = new int[coins.length][amount+1];
         for (int j = 1; j <= amount; j++) {
-            if (coins[0] <= j && dp[0][j-coins[0]] != Integer.MAX_VALUE)
+            if (coins[0] <= j && dp[0][j-coins[0]] != -1)
                 dp[0][j] = 1+dp[0][j-coins[0]];
-            else dp[0][j] = Integer.MAX_VALUE;
+            else dp[0][j] = -1;
         }
         for (int i = 1; i < coins.length; i++) {
             for (int j = 1; j <= amount; j++) {
-                if (coins[i] <= j && dp[i][j-coins[i]] != Integer.MAX_VALUE)
-                    dp[i][j] = Math.min(dp[i-1][j], 1+dp[i][j-coins[i]]);
+                if (coins[i] <= j && dp[i][j-coins[i]] != -1)
+                    dp[i][j] = 1+dp[i][j-coins[i]];
                 else dp[i][j] = dp[i-1][j];
+                if (dp[i-1][j] != -1) dp[i][j] = Math.min(dp[i-1][j], dp[i][j]);
             }
         }
-        return dp[coins.length-1][amount] == Integer.MAX_VALUE ? -1 : dp[coins.length-1][amount];
+        return dp[coins.length-1][amount];
     }
 }
