74f4f290535e38a8a3a954e0dd22603c42197da1
anyulu <450801626@qq.com>
2020-11-25 17:13:07 -0800

    Lowest Common Ancestor of a Binary Tree

---
+++ 236.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    TreeNode res = null;
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        dfs(root, p, q);
+        return res;
+    }
+
+    private boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return false;
+        boolean left = dfs(root.left, p, q);
+        boolean right = dfs(root.right, p, q);
+        if ((left && right) ||
+            (root.val == p.val && (left || right)) ||
+            (root.val == q.val && (left || right))) {
+            if (res == null) res = root;
+            return true;
+        }
+        return left || right || root.val == p.val || root.val == q.val;
+    }
+}
