08ab06ced9fd11456931efd75ef7633a86d7655d
anyulu <450801626@qq.com>
2020-12-30 11:00:39 -0800

    Non-overlapping Intervals

--- 435.java
+++ 435.java
@@ -1,6 +1,6 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        if (intervals.length <= 1) return intervals.length;
+        if (intervals.length <= 1) return 0;
         Arrays.sort(intervals, (a,b) -> {return a[0] == b[0] ? a[1]-b[1] : a[0]-b[0];});
         int n = intervals.length;
         int res = 1;
@@ -8,6 +8,8 @@ class Solution {
         for (int i = 1; i < n; i++) {
             if (intervals[i][0] >= right) {
                 res++;
+                right = intervals[i][1];
+            } else if (intervals[i][1] < right) {
                 right = intervals[i][1];
             }
         }
