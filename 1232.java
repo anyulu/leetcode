30c9f9d1b3cf8979f43fa2150db91f276b9e059e
anyulu <450801626@qq.com>
2021-01-16 09:54:17 -0800

    Check If It Is a Straight Line

---
+++ 1232.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        int n = coordinates.length;
+        int x1 = coordinates[0][0];
+        int y1 = coordinates[0][1];
+        int x2 = coordinates[1][0];
+        int y2 = coordinates[1][1];
+        for (int i = 2; i < n; i++) {
+            int x = coordinates[i][0];
+            int y = coordinates[i][1];
+            if ((y2-y1)*(x-x1) != (y-y1)*(x2-x1)) return false;
+        }
+        return true;
+    }
+}
